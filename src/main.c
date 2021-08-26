9cbf2c08e2bb6bdef19b5ca21cc602c1ec2557c6
Damjan Belavic <damjan.belavic@vuka.hr>
2021-08-26 11:10:36 +0200

    Day 4: Minor update

--- src/main.c
+++ src/main.c
@@ -18,17 +18,26 @@
 int32_t main(void)
 {
     char *result = NULL;
-    
-    printf("|-------+-------------|\n");
-    printf("| Input | Output      |\n");
-    printf("|-------+-------------|\n");
-    for (int32_t n = 0; n <= 100; n++)
+    int32_t lower, upper; 
+
+    printf("Enter check range (lower upper): ");
+    if (scanf("%d %d", &lower, &upper) != 2)
+    {
+        printf("Invalid input.\n")
+        return (-1);
+    }
+
+    printf("Input | Output\n");
+
+    for (int32_t n = lower; n <= upper; n++)
     {
         result = check_divisibility(n);
-        printf("| %5d | %-11s |\n", n, result);
-        printf("|-------+-------------|\n");
+        if (result != NULL)
+        {
+            printf("%5d | %-11s\n", n, result);
+        }
     }
     free((void *)result);
 
-    exit(0);
+    return (0);
 }
